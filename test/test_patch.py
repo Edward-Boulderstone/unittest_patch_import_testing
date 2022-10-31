8040d5e0659590da8e20893820ec23f111188641
Edward-Boulderstone <Edward.Boulderstone@bjss.com>
2022-10-31 11:47:13 +0000

    Renamed non-test method

--- test/test_patch.py
+++ test/test_patch.py
@@ -8,11 +8,11 @@ def patch_method():
     return "B"
 
 
-@patch("namespace_testing.method_holder.original_method", test_method)
+@patch("namespace_testing.method_holder.original_method", patch_method)
 def test_from_import():
     assert namespace_testing.import_namespace.run_method() == "B"
 
 
-@patch("namespace_testing.method_holder.original_method", test_method)
+@patch("namespace_testing.method_holder.original_method", patch_method)
 def test_import_namespace():
     assert namespace_testing.from_import.run_method() == "B"
