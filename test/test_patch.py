c217deea801ca6f1e16789996fc2e3992e3a4777
Edward-Boulderstone <Edward.Boulderstone@bjss.com>
2022-11-01 14:56:24 +0000

    Changed test to use mock instead of patch

--- test/test_patch.py
+++ test/test_patch.py
@@ -1,18 +1,13 @@
-from unittest.mock import patch
+from unittest.mock import Mock
 import namespace_testing.method_holder
 import namespace_testing.import_namespace
 import namespace_testing.from_import
 
-
-def patch_method():
-    return "B"
-
-
-@patch("namespace_testing.method_holder.original_method", patch_method)
 def test_from_import():
+    namespace_testing.method_holder.original_method = Mock(return_value='B')
     assert namespace_testing.import_namespace.run_method() == "B"
 
 
-@patch("namespace_testing.method_holder.original_method", patch_method)
 def test_import_namespace():
+    namespace_testing.method_holder.original_method = Mock(return_value='B')
     assert namespace_testing.from_import.run_method() == "B"
