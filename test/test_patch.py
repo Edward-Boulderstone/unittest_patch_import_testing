bbaedfddfe1581274f660d7a0b320e783ce3f9f4
Edward-Boulderstone <Edward.Boulderstone@bjss.com>
2022-11-01 16:10:19 +0000

    Added suggested import

--- test/test_patch.py
+++ test/test_patch.py
@@ -2,12 +2,15 @@ from unittest.mock import Mock
 import namespace_testing.method_holder
 import namespace_testing.import_namespace
 import namespace_testing.from_import
+from namespace_testing.from_import import run_method
+
+def test_import_namespace():
+    namespace_testing.method_holder.original_method = Mock(return_value='B')
+    assert run_method() == "B"
 
 def test_from_import():
     namespace_testing.method_holder.original_method = Mock(return_value='B')
     assert namespace_testing.import_namespace.run_method() == "B"
 
 
-def test_import_namespace():
-    namespace_testing.method_holder.original_method = Mock(return_value='B')
-    assert namespace_testing.from_import.run_method() == "B"
+
