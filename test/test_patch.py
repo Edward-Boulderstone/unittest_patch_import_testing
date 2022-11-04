59b9a316cb20d68b46c60ac8b5deab4341609854
Edward-Boulderstone <Edward.Boulderstone@bjss.com>
2022-11-04 10:27:32 +0000

    Update test_patch.py

--- test/test_patch.py
+++ test/test_patch.py
@@ -12,14 +12,7 @@ def patch_method():
 def test_import_namespace():
     assert namespace_testing.import_namespace.run_method() == "B"
 
-@patch("namespace_testing.method_holder.original_method", patch_method)
+@patch("namespace_testing.from_import.original_method", patch_method)
 def test_from_import():
     # Comment out top level import and uncomment this import for some funky behaviour
-    # import namespace_testing.import_namespace
-    assert namespace_testing.from_import.run_method() == "B"
-
-@patch("namespace_testing.from_import.original_method", patch_method)
-def test_patch_different_methodfrom_import():
-    # Comment out top level import and uncomment this import for some funky behaviour
-    # import namespace_testing.import_namespace
     assert namespace_testing.from_import.run_method() == "B"
